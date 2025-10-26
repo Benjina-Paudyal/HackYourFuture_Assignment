f95ce7bda0e8e72f9320e76f0abf7e3c4c1f2659
Benjina Paudyal <benjina.paudyal@gmail.com>
2025-10-26 20:44:54 +0100

    Added basic javascript to change the background color randomly

---
+++ AI/week1/main.js
@@ -0,0 +1,4 @@
+document.getElementById("colorChangeButton").addEventListener("click", () => {
+  const randomColor = `hsl(${Math.floor(Math.random() * 360)}, 70%, 80%)`;
+  document.body.style.backgroundColor = randomColor;
+});
