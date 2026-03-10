33e21b6e8bfa3272b8a4e0105ee838a6ebf3f154
Benjina Paudyal <benjina.paudyal@gmail.com>
2026-03-10 10:35:12 +0100

    Exercise1: minor fix (removed extra bracket)

--- advanced-JavaScript/week1-assignment/exercise1.js
+++ advanced-JavaScript/week1-assignment/exercise1.js
@@ -1,8 +1,9 @@
+// Exercise 1 : Rewrite with array methods
+
 import { teas } from "../data/teas.js";
 
-// Exercise 1
 const result = teas
-  .filter((tea) => tea.caffeineLevel !== "none")
-  .map((tea) => tea.name.toUpperCase());
+  .filter(tea => tea.caffeineLevel !== "none")
+  .map(tea => tea.name.toUpperCase());
 
 console.log(result);
