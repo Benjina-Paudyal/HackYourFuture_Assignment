4f451d86593bfca318797e6adf38cd5029b1ee74
Benjina Paudyal <benjina.paudyal@gmail.com>
2026-03-11 11:56:55 +0100

    fix: fix code after mentor review

--- advanced-JavaScript/week1-assignment/exercise2.js
+++ advanced-JavaScript/week1-assignment/exercise2.js
@@ -4,14 +4,16 @@ import { teas } from "../data/teas.js";
 
 function inventoryReport(teas) {
   const totalTeas = teas.length;
+
   const inStock = teas.filter(tea => tea.inStock).length;
   const outOfStock = totalTeas - inStock;
+
   const totalInventoryValue = Number(
     teas
       .reduce((total, tea) => total + tea.pricePerGram * tea.stockCount, 0)
       .toFixed(2),
   );
-  const averagePrice = Number((totalInventoryValue / totalTeas).toFixed(2));
+  const averagePrice = Number((teas.reduce((sum, tea) => sum + tea.pricePerGram, 0) / totalTeas).toFixed(2));
 
   return {
     totalTeas,
